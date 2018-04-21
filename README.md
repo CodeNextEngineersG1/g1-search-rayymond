062de35fad94723d226446ebf910d845969e8b3f
rayymond <32797293+rayymond@users.noreply.github.com>
2018-04-21 12:44:52 -0400

    part 4

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # Project Guide
 https://docs.google.com/document/d/1ipqrzyqz0NNYHzIOrG8f-abmvsGsSIPCd0zyWDHywJw/edit?usp=sharing
+
+ {
+     {
+    name:"Chimchar",
+    type:["Fire"],
+    generation:"4",
+    evolutions:["Monferno", "Infernape"],
+    moves: ["Scratch", "Ember"]
+  },
+
+  {
+
+    name:"Turtwig",
+    type:"Grass",
+    generation:"4",
+    evolutions:["Grotle", "Torterra"],
+    moves: ["Tackle", "Vine Whip"]
+  },
+
+   {
+ 	name:"Piplup",
+    type:["Water"],
+    generation:"4",
+    evolutions:["Prinplup", "Empoleon"],
+    moves: ["Pound", "Water Gun"]
+
+   },
+   {
+    name:"Starly",
+    type:["Normal","Flying"],
+    generation:"4",
+    evolutions:["Staravia", "Staraptor"],
+    moves: ["Tackle", "Peck"]
+	},
+
+	{
+ 	name:"Shinx",
+    type:["Electric"],
+    generation:"4",
+    evolutions:["Luxio", "Luxray"],
+    moves: ["Tackle", "Spark"]
+
+	
+];
--- script.js
+++ script.js
@@ -1,1 +1,60 @@
 /* write your script here */ 
+var database = [
+ {
+    name:"Chimchar",
+    type:["Fire"],
+    generation:"4",
+    evolutions:["Monferno", "Infernape"],
+    moves: ["Scratch", "Ember"]
+  },
+
+  {
+
+    name:"Turtwig",
+    type:"Grass",
+    generation:"4",
+    evolutions:["Grotle", "Torterra"],
+    moves: ["Tackle", "Vine Whip"]
+  },
+
+   {
+ 	name:"Piplup",
+    type:["Water"],
+    generation:"4",
+    evolutions:["Prinplup", "Empoleon"],
+    moves: ["Pound", "Water Gun"]
+
+   },
+   {
+    name:"Starly",
+    type:["Normal","Flying"],
+    generation:"4",
+    evolutions:["Staravia", "Staraptor"],
+    moves: ["Tackle", "Peck"]
+	},
+
+	{
+ 	name:"Shinx",
+    type:["Electric"],
+    generation:"4",
+    evolutions:["Luxio", "Luxray"],
+    moves: ["Tackle", "Spark"]
+
+];
+
+
+ var searchBar = document.getElementbyId("searchBar")
+
+ var searchButton = document.getElementbyId("searchButton")
+
+ var autoSuggestions = document.getElementbyId("autoSuggestions")
+
+ var display = document.getElementbyId("display") 
+
+ searchBar.addEventListener("keypress", checkKey)
+
+ searchButton.addEventListener("click", processInput)
+
+
+
+
